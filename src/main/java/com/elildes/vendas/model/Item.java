5a16e25868a647b3eca6f6ceb2f19f9f450d8e40
Elildes F Santos <colafina2000@yahoo.com.br>
2022-12-06 08:25:38 -0300

    update

--- src/main/java/com/elildes/vendas/model/Item.java
+++ src/main/java/com/elildes/vendas/model/Item.java
@@ -2,29 +2,40 @@
 
 package com.elildes.vendas.model;
 
+//import javax.persistence.Entity;
+//import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Item {
 
 	private int num;
 	private String nome;
 	
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
-	}
-	
-	public String getNome() {
-		return nome;
-	}
-	
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-	
-	public Item(int num, String nome) {
-		super();
-		this.num = num;
-		this.nome = nome;
-	}	
+//	public int getNum() {
+//		return num;
+//	}
+//	public void setNum(int num) {
+//		this.num = num;
+//	}
+//	
+//	public String getNome() {
+//		return nome;
+//	}
+//	
+//	public void setNome(String nome) {
+//		this.nome = nome;
+//	}
+//	
+//	public Item(int num, String nome) {
+//		super();
+//		this.num = num;
+//		this.nome = nome;
+//	}	
 }
