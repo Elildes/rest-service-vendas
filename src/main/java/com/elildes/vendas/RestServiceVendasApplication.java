0f6f07d091b8abd0b969d02a82e4dc5c1f37cbd1
Elildes F Santos <colafina2000@yahoo.com.br>
2022-12-06 08:25:22 -0300

    update

--- src/main/java/com/elildes/vendas/RestServiceVendasApplication.java
+++ src/main/java/com/elildes/vendas/RestServiceVendasApplication.java
@@ -3,6 +3,11 @@ package com.elildes.vendas;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 5.6 Criar uma Api com Spring JPA:
+ * Prática da aula 19 Spring - REST Api
+ * Aplicação MVC no Spring com parâmetro
+ */
 @SpringBootApplication
 public class RestServiceVendasApplication {
 
